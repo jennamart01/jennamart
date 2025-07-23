48ecdc602c36ad4cab8d676bd91799cea2d821c5
mj-rahmadwijaya <rahmad.wijaya@magic-jungle.co.id>
2025-07-23 10:33:09 +0700

    build

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "FreePos - Simple POS System",
@@ -35,7 +33,7 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children}
       </body>
     </html>
