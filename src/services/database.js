8946b00942e7c36f7d9ac8cddb29b58a0a3425c5
mj-rahmadwijaya <rahmad.wijaya@magic-jungle.co.id>
2025-07-23 11:23:48 +0700

    log env

--- src/services/database.js
+++ src/services/database.js
@@ -6,6 +6,9 @@ let clientPromise;
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your MongoDB URI to .env.local');
 }
+console.log('env----',process.env.NODE_ENV);
+console.log('url------', process.env.MONGODB_URI);
+
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable to preserve the value
